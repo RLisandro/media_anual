cc22b5a083e97b47f81e029a4c95b21374015ae0
LISANDRO REZENDE CAZUZA <rezendecazuza@gmail.com>
2025-03-27 19:56:57 -0300

    Data de nascimento

---
+++ calcula_idade.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+data_nascimento = input("Digite sua data de nascimento: (dd/mm/aa):  ")
+nasc = datetime.strptime(data_nascimento,"%d/%m/%Y")
+hoje = datetime.today() 
+idade = hoje.year  -  nasc.year
+print(f"Sua idade é: {idade} ano.")
+
